3f46b54d80a0c25e66b4b63e9b287ec42fa79d34
Kiran Brahmatewari <kiwi2mii@gmail.com>
2024-12-05 20:37:34 -0500

    Update cart prices + names

--- scripts/cart.js
+++ scripts/cart.js
@@ -170,8 +170,7 @@ function displayCart() {
 
   for(var i in cartArray) {
     output += "<tr>"
-      + "<td>" + cartArray[i].name + "</td>" 
-      + "<td>(" + cartArray[i].price + ")</td>"
+      + "<td>" + cartArray[i].name.replaceAll("_", " ") + "</td>" 
       + "<td><div class='input-group'><button class='minus-item input-group-addon btn btn-primary' data-name=" + cartArray[i].name + ">-</button>"
       + "<input type='number' class='item-count form-control' data-name='" + cartArray[i].name + "' value='" + cartArray[i].count + "'>"
       + "<button class='plus-item btn btn-primary input-group-addon' data-name=" + cartArray[i].name + ">+</button></div></td>"
